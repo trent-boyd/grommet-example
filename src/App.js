3a009971e2b106c8f252df88c5072fbce59c57f1
Trent Boyd <trent.boyd@gmail.com>
2020-02-26 19:07:29 -0600

    feat: add mobile sidebar

--- src/App.js
+++ src/App.js
@@ -1,43 +1,81 @@
-import React from "react";
-import { Box, Button, Grommet, Heading } from "grommet";
-import { Notification } from "grommet-icons";
+import React from 'react'
+import {
+  Box,
+  Button,
+  Collapsible,
+  Grommet,
+  Heading,
+  Layer,
+  ResponsiveContext,
+} from 'grommet'
+import { FormClose, Notification } from 'grommet-icons'
 
-import AppBar from "components/AppBar";
-import { theme } from "theme";
+import AppBar from 'components/AppBar'
+import { theme } from 'theme'
 
 function App() {
-  const [showSidebar, setShowSidebar] = React.useState(false);
+  const [showSidebar, setShowSidebar] = React.useState(false)
   return (
     <Grommet theme={theme} full>
-      <Box fill>
-        <AppBar>
-          <Heading level="3" margin="none">
-            Example Grommet App
-          </Heading>
-          <Button
-            icon={<Notification />}
-            onClick={() => setShowSidebar(!showSidebar)}
-          />
-        </AppBar>
-        <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
-          <Box flex align="center" justify="center">
-            APP BODY!
-          </Box>
-          {showSidebar && (
-            <Box
-              width="medium"
-              background="light-2"
-              elevation="small"
-              align="center"
-              justify="center"
-            >
-              SIDEBAR!
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level="3" margin="none">
+                Example Grommet App
+              </Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+            <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+              <Box flex align="center" justify="center">
+                APP BODY!
+              </Box>
+              {!showSidebar || size !== 'small' ? (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    elevation="small"
+                    align="center"
+                    justify="center"
+                  >
+                    SIDEBAR!
+                  </Box>
+                </Collapsible>
+              ) : (
+                <Layer>
+                  <Box
+                    background="light-2"
+                    tag="header"
+                    justify="end"
+                    align="center"
+                    direction="row"
+                  >
+                    <Button
+                      icon={<FormClose />}
+                      onClick={() => setShowSidebar(false)}
+                    />
+                  </Box>
+                  <Box
+                    fill
+                    background="light-2"
+                    align="center"
+                    justify="center"
+                  >
+                    MOBILE SIDEBAR!
+                  </Box>
+                </Layer>
+              )}
             </Box>
-          )}
-        </Box>
-      </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
     </Grommet>
-  );
+  )
 }
 
-export default App;
+export default App
